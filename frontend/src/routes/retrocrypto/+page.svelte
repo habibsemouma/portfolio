d9aaf8a3bafed82ac639b956c458df0d4bf6e565
Habib <semoumahabib@gmail.com>
2023-09-25 15:45:53 +0100

    only video and css fix left to add

--- frontend/src/routes/retrocrypto/+page.svelte
+++ frontend/src/routes/retrocrypto/+page.svelte
@@ -1,8 +1,10 @@
 <script>
   import {Body} from 'svelte-body'
   import axios from "axios"
+  import {Circle2} from "svelte-loading-spinners"
     export let current_encoding = "Choose an encoding";
     export let output_text="";
+    let data_loading=false
     function change_encoding(encoding) {
       current_encoding = encoding;
     }
@@ -18,10 +20,12 @@
           console.log(error)
 
         }
+        data_loading=false
       }
     
   
     async function prepare_data(type) {
+      data_loading=true
       let data={
           "text":document.getElementById('inptext').value,
           "encoding":current_encoding,
@@ -47,12 +51,12 @@
   </script>
   
   <head />
- <Body style=" display: flex; flex-direction: column; justify-content: center;align-items: center;gap: 40px;background-color: #F2F4F6;font-family: Arial, Helvetica, sans-serif;color:black;margin: 0;padding: 0;" />
+ <Body style=" display: flex; flex-direction: column;gap:20px; justify-content: center;align-items: center;gap: 40px;background-color: #F2F4F6;font-family: Arial, Helvetica, sans-serif;color:black;margin: 0;padding: 0;" />
     <div>
       <h1>{current_encoding}</h1>
     </div>
   
-    <div>
+    <div id="handles">
       <button
         on:click={() => {
           change_encoding("caesar");
@@ -88,7 +92,7 @@
         }} id="decodebtn">Decode</button>
     </div>
   
-    <div>
+    <div id="dynamicoptions">
       {#if current_encoding == "Choose an encoding"}
         <p />
       {:else if current_encoding == "caesar"}
@@ -101,10 +105,16 @@
         <input id="keyword" placeholder="Keyword" />
       {/if}
     </div>
+    
+    <div style="display: flex;justify-content: center;align-items: center;">
+      {#if data_loading}
+      <Circle2 size="60" unit="px" />
+      {/if}
+    </div>
   
     <div id="docs">
-      <textarea placeholder="text goes here" class="textdisp" id="inptext" />
-      <textarea readonly placeholder="result goes here" class="textdisp" id="outtext" value="{output_text}" />
+      <textarea placeholder="Text goes here" class="textdisp" id="inptext" />
+      <textarea readonly placeholder="Result goes here" class="textdisp" id="outtext" value="{output_text}" />
     </div>
  
   
@@ -113,6 +123,7 @@
  
 
     .textdisp{
+      border-radius: 10px;
       height: 500px;
       width: 300px;
       padding:10px;
@@ -135,6 +146,7 @@
       padding: 1rem;
       border-radius: 2rem;
       color:white;
+      font-size: 20px;
     }
     .handle:hover{
       transform: translateY(-5px);
@@ -145,6 +157,23 @@
     }
     #decodebtn{
       background-color: red;
+    }
+    #handles{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap:20px;
+    }
+    #docs{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    #dynamicoptions{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
     }
     input{
       color:black;
@@ -166,6 +195,7 @@
   margin-left: 0;
   margin-right: 0;
   font-weight: bold;
+  text-align: center;
     }
   </style>
   
